7fa60aadbdf8091444759547e9bf47028cf02eb1
Abhishek180007 <abh353739@gmail.com>
2025-01-14 22:53:35 +0530

    Adding Project

---
+++ Aws_Instance_Prediction_Notebooks/AWS_Instance_Prediction/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9476292cbd8fd39e1a9f2bae25f60bd50fcf8354697a42d1cd0d81a91ab74d8a
+size 2032
---
+++ Aws_Instance_Prediction_Notebooks/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4fdb655e2fe9f814eadf2c8235be674e6014475aa07a078fddeb785bfafc3e77
+size 2013
